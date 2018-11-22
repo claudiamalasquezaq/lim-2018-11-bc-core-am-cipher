7c7f27b1d94aba747e710b02069ecc043fe1a165
unknown <claudiamalasquez67@gmail.com>
2018-11-22 12:29:52 -0500

    Create the cipherEncode function and add it to he click event

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const btnEncode = document.getElementById("btn-encode");
 const btnDecode = document.getElementById("btn-decode");
 const btnReturn = document.getElementById("btn-return");
 //Cajas de texto
-const numOffset = document.getElementById("num-offset");
-const msgEncryp = document.getElementById("msg-encryp");
+
 const msgAnswer = document.getElementById("msg-answer");
 
 
@@ -27,5 +26,42 @@ function returnHome() {
   scrGoodBye.classList.add("oculto");
 }
 
+function cipherEncode(msgEncryp,numOffset){
+  	let cadena = "";
+	for(let i = 0; i < msgEncryp.length; i++){
+	    let pAsc = msgEncryp.charCodeAt(i);
+	    if (pAsc === 32){
+	      cadena = cadena + " ";
+	      console.log(cadena);
+	    } else {
+		  let fCifrar = (pAsc - 65 + parseInt(numOffset)) % 26 + 65;
+		  let palCifrada = String.fromCharCode(fCifrar);
+		  cadena = cadena + palCifrada;
+		  console.log(cadena);
+		//}
+	}
+	msgAnswer.innerHTML = cadena;	
+}
+}
+
+function sendDecrypted() {
+
+	if (numOffset.value <= 0){
+		alert('Ingresar un número positivo');
+	} else if (numOffset.value === 0) {
+		alert('Si ingresas como código el número 0, el texto no podrá ser cifrado');
+	} else {
+		cipherEncode();
+	}
+}
+
+
+
 btnStart.addEventListener("click", apSgtePantalla);
 btnReturn.addEventListener("click", returnHome);
+btnEncode.addEventListener("click", () => {
+	const numOffset = document.getElementById("num-offset").value;
+	const msgEncryp = document.getElementById("msg-encryp").value;
+
+	cipherEncode(msgEncryp, numOffset);
+});
