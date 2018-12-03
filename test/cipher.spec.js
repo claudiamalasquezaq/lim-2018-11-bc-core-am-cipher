2205d481522c57389d8738ff3d64bf80017fc078
unknown <claudiamalasquez67@gmail.com>
2018-12-03 09:49:27 -0500

    Test encode and decode with spaces

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -14,6 +14,11 @@ describe('cipher', () => {
       assert.equal(cipher.encode(33,'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),'HIJKLMNOPQRSTUVWXYZABCDEFG');
     }
     );
+
+    it('debería retornar "IPMB RVF UBM" para "HOLA QUE TAL" con offset 1', () => {
+      assert.equal(cipher.encode(1,'HOLA QUE TAL'),'IPMB RVF UBM');
+    });
+
   });
 
   describe('cipher.decode', () => {
@@ -24,6 +29,10 @@ describe('cipher', () => {
 
     it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
       assert.equal(cipher.decode(33,'HIJKLMNOPQRSTUVWXYZABCDEFG'),'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+    });
+
+    it('debería retornar "HOLA QUE TAL" para "IPMB RVF UBM" con offset 1', () => {
+      assert.equal(cipher.decode(1, 'IPMB RVF UBM'),'HOLA QUE TAL');
     });
 
   });
